8e4e2c43d0f91e81f82dcb2d777e575a8151608e
noe <noe.tehraoui1@gmail.com>
2023-08-16 14:44:37 +0200

    é

--- front/JS/product.js
+++ front/JS/product.js
@@ -1,11 +1,12 @@
+// récupération de l'id dans l'url
 var str = window.location.href;
 var url = new URL(str);
 var id = url.searchParams.get("id");
 //console.log(id);
-
+// construction du chemin qui mène aux éléments spécifique à chaque produit
 var apiURL = "http://localhost:3000/api/products/" + id;
 console.log(apiURL);
-
+// récupération des éléments des produits via l'api
 fetch(apiURL)
     .then((response) => {
         response.json()
