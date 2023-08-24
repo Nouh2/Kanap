de9a6b88ed1571c98b99f0f3f8e327fccb2a25b1
noe <noe.tehraoui1@gmail.com>
2023-08-24 12:32:15 +0200

    no code

--- front/JS/product.js
+++ front/JS/product.js
@@ -17,9 +17,45 @@ fetch(apiURL)
     item.querySelector("#price").insertAdjacentHTML("afterbegin", data.price);
     item.querySelector("#description").insertAdjacentHTML("afterbegin", data.description);
     item.querySelector("#colors").insertAdjacentHTML("beforeend", data.colors.map(colors => `<option value ="${colors}">${colors}</option>`).join(''));
-    //stockage du choix du client
-    localStorage.setItem("product", id);
-    localStorage.setItem("color", colors);
-    localStorage.setItem("quantity", itemQuantity);
+    
+
+    //cart = tableau avec l'id, la quantité, la couleur
+
+    const cart = [id, quantity, color];
+
+    //ajout new product dans le cart (submit) -- if new product n'est pas dans le tableau on add 
+    
+    const checkItemInCart = (id, color) => {
+
+        //récupération de la data du cart
+        cart;
+        //if data isn't in the cart = true add item
+        if(id = id.cart, color = color.cart){
+            return false;
+        }
+            return true;
+    }
+    console.log(checkItemInCart);
+
+    const addItemToCart = () => {
+
+        //récupération de l'id, de la colors et de la quantité
+        //eventlistener qui submit lors lorsque l'on clique sur le boutton
+
+    }
+
+    
+    //ajout new product dans la cart if new product est deja dans le panier (id + color) on ++ quantité
+
+    const addNewItemToCart = (id, color) => {
+
+        //récupération de la data du cart
+        //if cart[id, color] = choix client ++ quantité
+
+    }
+
+    const quantity = 3;
+    const color = red;
+
 })
 
