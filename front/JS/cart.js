ed4dd9bf1bc84f297eda67fde1ec4ef6df4c10f8
noe <noe.tehraoui1@gmail.com>
2023-10-05 17:15:11 +0200

    ajout fonction delete quantity

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,21 +1,21 @@
 let cart = window.localStorage.getItem("cart");
 //console.log(cart);
 let cartArray = JSON.parse(cart);
-console.log(cartArray);
+//console.log(cartArray);
 
     if (cartArray === null ){
         const cartVideHTML = document.getElementById('cart__items');
         cartVideHTML.innerHTML = "Votre panier est vide";
     } else{
     for (const element of cartArray){
-        console.log(element);
-        fetch('http://localhost:3000/api/products/')
+        //console.log(element);
+        fetch('http://localhost:3000/api/products/' + element.id)
         .then(response => response.json())
         .then(products => {
             //console.log(products);
         const article = document.createElement("article");
         document.querySelector("#cart__items").appendChild(article);
-        article.className = "cart_item";
+        article.className = "cart__item";
         article.setAttribute("data-id", element.id);
         article.setAttribute("data-color", element.color);
         
@@ -58,26 +58,34 @@ console.log(cartArray);
         let quantity = document.createElement("p");
         contentSettingsQuantityDiv.appendChild(quantity);
         quantity.innerHTML = "Qté :" + element.quantity;
+
+        changeProductQantity();
     })}}
+//change quantity grace à l'input qui permet de selectionner le produit que l'on veut modifier
+function changeProductQantity(input, product) {
+    input.onchange = (event) => {
+        const newquantity = event.target.value;
+        product.quantity = newquantity;
+        event.target.previousElementSibling.textContent = "Qté :" + product.quantity;
+
+        const cart = JSON.stringify(cart);
+        localStorage.setItem("cart", cart);
+    }
+}
 
 
-
- 
+//changer la quantité par rapport à l'id et a la color
+//{
+    //récupération de la data id et color dans l'élément parent cart__item
+    //const cart_item = event.target.closest("cart__item")
+    //const id = cart_item.dataset.id
+    //console.log(id);
+    //const color = cart_item.dataset.color
+    // trouver le id et la color 
+    ////cart.find((element) => element.id === id && element.color === color ).quantity = event.target.value
+//}
+//console.log(changeProductQantity);
 
    
 
 
-//for (const{id, quantity, color} of cartArray){
-
-    //const apiURL = "http://localhost:3000/api/products/" + id;
-    //const {name,  _id, imageUrl, alttxt, description } =  fetch(apiURL)
-    //console.log(name);
-    //const Items = document.getElementById("cart__items");
-        //const cartItem = document.createElement("cart__item");
-        //Items.appendChild(cartItem);
-
-        //const img = document.createElement("cart__item__img");
-        //img.src = imageUrl;
-        //cartItem.appendChild(img);
-
-//}
